c71f711490b97e3df450f25a8b170ef092fbdd6a
DistNovelist <apps.dist.892@gmail.com>
2021-10-03 18:03:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import pygame.midi as m
 import time
 import keyboard as key
+import threading
 import tkinter
 notename = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"]
+def gui():
+    tk = tkinter.Tk()
+    tk.geometry("600x200")
+    tk.title("Auto-magic MIDI Keyboard")
+
+    tk.mainloop()
+
+def keyprocess():
+    while True:
+        if key.on_press
+
 def main():
-    print("Virtual MIDI Magic v0.1 by DIST_NOVELIST")
+    print("Auto-magic MIDI Keyboard v0.1 by DIST_NOVELIST")
     m.init()
     print(str(m.get_default_output_id())+"/input:"+str(m.get_default_input_id()))
     i_num = m.get_count()
@@ -25,7 +37,7 @@ def main():
             print ("full midi_events:" + str(midi_events) + ", note:" + notename[midi_events[0][0][1]%12] + str(midi_events[0][0][1]//12-2))
             count += 1
         if count >= 14:
-            going = False"""
+            going = False
     midiout = m.Output(1)
     midiout.note_on(60,100)
     time.sleep(1)
@@ -44,14 +56,23 @@ def main():
     midiout.note_off(60,100)
     midiout.note_off(64,100)
     midiout.note_off(67,100)
-
-    """time.sleep(1)
+    time.sleep(1)
     midiout.note_off(60)
     time.sleep(1)
     midiout.note_off(64)
     time.sleep(1)
     midiout.note_off(67)"""
     #i.close()
+
+    guiTh = threading.Thread(target=gui)
+    keyTh = threading.Thread(target=keyprocess)
+    guiTh.start()
+    keyTh.start()
+
+    guiTh.join()
+    keyTh.join()
+
+    midiout = m.Output(1)
     midiout.close()
     m.quit()
     exit()
