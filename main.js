9aa3abde40a4edb68889c201070b13d4a75a289c
DistNovelist <apps.dist.892@gmail.com>
2021-11-06 12:14:12 +0900

    ピアノロールクリックでのMIDIイベント発火を可能に

--- main.js
+++ main.js
@@ -1,4 +1,56 @@
+var midi = null;
+var input_devices = [];
+var output_devices = [];
+
+//jquery読み込み
 jQuery = $ = require('jquery');
+//web midi使用
+navigator.requestMIDIAccess().then(onMIDISuccess,onMIDIFailure);
+function onMIDIFailure(msg){
+    console.log("onMIDIFailure(): "+msg);
+}
+function onMIDISuccess(m){
+    midi=m;
+    //MIDI I/Oデバイス列挙
+    var it = midi.inputs.values();
+    for(var o = it.next(); !o.done; o = it.next()){
+      input_devices.push(o.value);
+    }
+    var ot = midi.outputs.values();
+    for(var o = ot.next(); !o.done; o = ot.next()){
+      output_devices.push(o.value);
+    }
+    console.log(input_devices);
+    console.log(output_devices);
+}
+
+//MIDIノート信号送信
+function MidiNoteOn(note, vel){
+    if(output_devices.length > 0){
+        console.log("note on")
+        output_devices[0].send([0x90,note,vel]);
+    }
+}
+//MIDIノートオフ信号送信
+function MidiNoteOff(note){
+    if(output_devices.length > 0){
+        console.log("note off")
+        output_devices[0].send([0x90,note,0x00]);
+    }
+}
+mousePressedKeys=[];
+function keyMousePressed(e){
+    noteNum = e.data.note;
+    MidiNoteOn(noteNum,100);
+    mousePressedKeys.push(noteNum);
+}
+function keyMouseReleased(){
+    mousePressedKeys.forEach(e => {
+        MidiNoteOff(e);
+    });
+    mousePressedKeys=[];
+}
+
 var keynames = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"];
 var blackkeys = [1,3,6,8,10];
 $(function(){
@@ -11,14 +63,9 @@ $(function(){
             $("#piano").append("<div class='piano_key' id='"+i.toString()+"'>"+keynames[i%12]+Math.floor(-2+i/12).toString()+"</div>");
         }
         //キーがマウスで押されたときの処理
-        $("#"+i.toString()).on("mousedown",function(){
-            console.log(i.toString()+" pressed");
-        });
+        $("#"+i.toString()).on("mousedown",{note: i},keyMousePressed);
         //キーが離されたときの処理
-        $("#"+i.toString()).on("mouseup",function(){
-            console.log(i.toString()+" released");
-        });
+        $("#"+i.toString()).on("mouseup",keyMouseReleased);
     }
-
     
 });
